e101e56c5f3334c4f7bbb9fc961a46cc8d0fa6c3
lewol <76611386+eronlowell@users.noreply.github.com>
2023-03-13 09:17:04 +0800

    Update find-job.php

--- views/find-job.php
+++ views/find-job.php
@@ -83,12 +83,14 @@ $page_first_result = ($page - 1) * $results_per_page;
       </div>
       <div class="col-6 my-auto">
         <div class="input-group pr-auto">
+          <form action="" method="GET">
           <div class="form-outline">
-            <input type="search" id="form1" class="form-control" placeholder="Search here.." />
+            <input type="search" name="search" id="search" class="form-control" placeholder="Search here.." />
           </div>
           <button type="button" class="btn btn-danger">
             <i class="bi bi-search"></i>
           </button>
+          </form>
         </div>
       </div>
     </div>
@@ -97,8 +99,13 @@ $page_first_result = ($page - 1) * $results_per_page;
     <div class="row">
 
       <?php
-
+      $search = $_GET['search'];
+      if ($search != ''){
       $sql = "SELECT *FROM jobpost LIMIT " . $page_first_result . ',' . $results_per_page;
+    }
+    else{
+      $sql = "SELECT *FROM jobpost WHERE job_title LIKE '%.$search.%' LIMIT " . $page_first_result . ',' . $results_per_page;
+    }
       $all_jobs = mysqli_query($conn, $sql);
       // $row = mysqli_fetch_assoc($all_jobs);
       while ($row = mysqli_fetch_assoc($all_jobs)) {
